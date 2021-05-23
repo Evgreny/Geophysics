75f3a6e54aec86896ddc90b6d7a569061ba6e4c6
Evgreny <EvgrenyPop@mail.ru>
2021-05-23 04:08:17 +0300

    restructuring data_processing

--- data_processing.py
+++ data_processing.py
@@ -225,11 +225,6 @@ def dataset_former_acoustic(FileName, Param):
     Data = Data[blowout]
     Data.reset_index(inplace=True, drop=True)
 
-    # if Param == "TSTR" or "FANG":
-    #     TSTRFANGdrop = (Data["AG_Depth"] > 900)
-    #     Data = Data[TSTRFANGdrop]
-    #     Data.reset_index(inplace=True, drop=True)
-
     # plt.hist(Data["AG_Depth"], bins=10)
     # plt.title(Param)
     # plt.show()
@@ -239,128 +234,87 @@ def dataset_former_acoustic(FileName, Param):
 
     encoder = OneHotEncoder(sparse=False)
     LitEncoder = pd.DataFrame(encoder.fit_transform(Data[["A_Краткая литологическая характеристика"]]))
+    LitEncoder = pd.DataFrame(encoder.transform(Data[["A_Краткая литологическая характеристика"]]))
+    litdict = dict(zip(list(LitEncoder.columns), encoder.categories_[0]))
+    LitEncoder = LitEncoder.rename(litdict, axis=1)
     scaler = StandardScaler()
     scale = pd.DataFrame(scaler.fit_transform(Data[["A_Пористость, Кп, %", "A_Плотность насыщенной породы, г/см3", "AG_Depth"]]))
     Dataset = pd.DataFrame({"por" : scale[0], "den" : scale[1], "depth" : scale[2], Param : Data[Param]})
     DatasetShuf = Dataset.join(LitEncoder)
     DatasetShuf.reset_index(inplace=True, drop=True)
-    # DatasetShuf = DatasetShuf.sample(frac=1, random_state=9)
-    # DatasetShuf.reset_index(inplace=True, drop=True)
-    # "Кросс валидация при различном перемешивании"
-    # a = np.array([])
-    # for i in range(1):
-    #     DatasetShuf = DatasetShuf.sample(frac=1, random_state=i)
-    #     # Обучение
-    #     X = DatasetShuf.drop(Param, 1)
-    #     y = DatasetShuf[Param]
-    #     # model = LinearRegression(normalize=False, fit_intercept=True)
-    #     model = CatBoostRegressor(depth=15, iterations=200, learning_rate=0.01)
-    #     cross = cross_val_score(model, X, y, cv=5, scoring="r2").mean()
-    #     print("Кросс-валидация: " + str(cross))
-    #     a = np.append(a, cross)
-    # print("Кросс валидация при различном перемешивании", a.mean())
+    return DatasetShuf
 
+
+def acoustic_learn(FileName, Param):
+
+    Data = dataset_former_acoustic(FileName, Param)
     # Обучение
     # model = LinearRegression(normalize=False, fit_intercept=True)
-    X = DatasetShuf.drop(Param, 1)
-    y = DatasetShuf[Param]
+    X = Data.drop(Param, 1)
+    y = Data[Param]
 
     if Param == "UCS":
-        modelRF = RandomForestRegressor(bootstrap=True, max_depth=15, max_features="auto", min_samples_leaf=5, min_samples_split=2, n_estimators=100, random_state=7) #UCS
+        modelRF = RandomForestRegressor(bootstrap=True, max_depth=15, max_features="auto", min_samples_leaf=5,
+                                        min_samples_split=2, n_estimators=100, random_state=7)  # UCS
         modelCAT = CatBoostRegressor(depth=5, iterations=200, l2_leaf_reg=1, learning_rate=0.01)
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.5, gamma=0.3, learning_rate=0.03, max_depth=3, min_child_weight=13, n_estimators=200, reg_alpha=0.75, reg_lambda=0.45, subsample=0.6) #UCS
-        modelKNN = KNeighborsRegressor(n_neighbors=5, n_jobs=-1) # UCS
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.5, gamma=0.3, learning_rate=0.03, max_depth=3,
+                                        min_child_weight=13, n_estimators=200, reg_alpha=0.75, reg_lambda=0.45,
+                                        subsample=0.6)  # UCS
+        modelKNN = KNeighborsRegressor(n_neighbors=5, n_jobs=-1)  # UCS
     elif Param == "TSTR":
-        modelRF = RandomForestRegressor(bootstrap=True, max_depth=11, max_features="auto", min_samples_leaf=5, min_samples_split=5, n_estimators=100, random_state=7)  # TSTR
-        modelCAT = CatBoostRegressor(depth=16, iterations=100, l2_leaf_reg=1, learning_rate=0.1) #TSTR
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.6, gamma=0.3, learning_rate=0.01, max_depth=4, min_child_weight=0, n_estimators=500, reg_alpha=None, reg_lambda=None, subsample=0.85) #TSTR
-        modelKNN = KNeighborsRegressor(n_neighbors=5, n_jobs=-1) # TSTR
+        modelRF = RandomForestRegressor(bootstrap=True, max_depth=11, max_features="auto", min_samples_leaf=5,
+                                        min_samples_split=5, n_estimators=100, random_state=7)  # TSTR
+        modelCAT = CatBoostRegressor(depth=16, iterations=100, l2_leaf_reg=1, learning_rate=0.1)  # TSTR
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.6, gamma=0.3, learning_rate=0.01, max_depth=4,
+                                        min_child_weight=0, n_estimators=500, reg_alpha=None, reg_lambda=None,
+                                        subsample=0.85)  # TSTR
+        modelKNN = KNeighborsRegressor(n_neighbors=5, n_jobs=-1)  # TSTR
     elif Param == "FANG":
-        modelRF = RandomForestRegressor(bootstrap=True, max_depth=11, max_features="auto", min_samples_leaf=1, min_samples_split=6, n_estimators=100, random_state=1)  # FANG
-        modelCAT = CatBoostRegressor(depth=12, iterations=150, learning_rate=0.04) # FANG
-        modelKNN = KNeighborsRegressor(n_neighbors=3, n_jobs=-1) # FANG
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.6, gamma=0.01, learning_rate=0.12, max_depth=5, min_child_weight=0, n_estimators=500, reg_alpha=3, reg_lambda=3, subsample=0.85) # FANG
+        modelRF = RandomForestRegressor(bootstrap=True, max_depth=11, max_features="auto", min_samples_leaf=1,
+                                        min_samples_split=6, n_estimators=100, random_state=1)  # FANG
+        modelCAT = CatBoostRegressor(depth=12, iterations=150, learning_rate=0.04)  # FANG
+        modelKNN = KNeighborsRegressor(n_neighbors=3, n_jobs=-1)  # FANG
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.6, gamma=0.01, learning_rate=0.12, max_depth=5,
+                                        min_child_weight=0, n_estimators=500, reg_alpha=3, reg_lambda=3,
+                                        subsample=0.85)  # FANG
     elif Param == "A_YME":
-        modelRF = RandomForestRegressor(bootstrap=False, max_depth=None, max_features="sqrt", min_samples_leaf=4, min_samples_split=3, n_estimators=100, random_state=1)  # A_YME
-        modelCAT = CatBoostRegressor(depth=11, iterations=500, learning_rate=0.02) #A_YME
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.4, gamma=0.3, learning_rate=0.1, max_depth=5, min_child_weight=0, n_estimators=150, reg_alpha=0.9, reg_lambda=None, subsample=0.04) # A_YME
-        modelKNN = KNeighborsRegressor(n_neighbors=2, n_jobs=-1) # A_YME
+        modelRF = RandomForestRegressor(bootstrap=False, max_depth=None, max_features="sqrt", min_samples_leaf=4,
+                                        min_samples_split=3, n_estimators=100, random_state=1)  # A_YME
+        modelCAT = CatBoostRegressor(depth=11, iterations=500, learning_rate=0.02)  # A_YME
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.4, gamma=0.3, learning_rate=0.1, max_depth=5,
+                                        min_child_weight=0, n_estimators=150, reg_alpha=0.9, reg_lambda=None,
+                                        subsample=0.04)  # A_YME
+        modelKNN = KNeighborsRegressor(n_neighbors=2, n_jobs=-1)  # A_YME
     elif Param == "A_PR":
-        modelRF = RandomForestRegressor(bootstrap=False, max_depth=None, max_features="sqrt", min_samples_leaf=4, min_samples_split=3, n_estimators=100, random_state=1) # A_PR
-        modelCAT = CatBoostRegressor(depth=15, iterations=500, learning_rate=0.01) #A_PR
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.4, gamma=0, learning_rate=0.05, max_depth=4, min_child_weight=30, n_estimators=500, reg_alpha=1e-05, reg_lambda=1e-05, subsample=1.0) # A_PR
-        modelKNN = KNeighborsRegressor(n_neighbors=22, n_jobs=-1) # A_PR
+        modelRF = RandomForestRegressor(bootstrap=False, max_depth=None, max_features="sqrt", min_samples_leaf=4,
+                                        min_samples_split=3, n_estimators=100, random_state=1)  # A_PR
+        modelCAT = CatBoostRegressor(depth=15, iterations=500, learning_rate=0.01)  # A_PR
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.4, gamma=0, learning_rate=0.05, max_depth=4,
+                                        min_child_weight=30, n_estimators=500, reg_alpha=1e-05, reg_lambda=1e-05,
+                                        subsample=1.0)  # A_PR
+        modelKNN = KNeighborsRegressor(n_neighbors=22, n_jobs=-1)  # A_PR
     elif Param == "M_YME":
-        modelRF = RandomForestRegressor(bootstrap=False, max_depth=10, max_features="sqrt", min_samples_leaf=2, min_samples_split=2, n_estimators=100, random_state=1)  # M_YME
+        modelRF = RandomForestRegressor(bootstrap=False, max_depth=10, max_features="sqrt", min_samples_leaf=2,
+                                        min_samples_split=2, n_estimators=100, random_state=1)  # M_YME
         modelCAT = CatBoostRegressor(depth=4, iterations=500, learning_rate=0.01)  # M_YME
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.8, gamma=0.01, learning_rate=0.15, max_depth=4, min_child_weight=0, n_estimators=100, reg_alpha=None, reg_lambda=None, subsample=0.75) # M_YME
-        modelKNN = KNeighborsRegressor(n_neighbors=2, n_jobs=-1) # M_YME
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=0.8, gamma=0.01, learning_rate=0.15, max_depth=4,
+                                        min_child_weight=0, n_estimators=100, reg_alpha=None, reg_lambda=None,
+                                        subsample=0.75)  # M_YME
+        modelKNN = KNeighborsRegressor(n_neighbors=2, n_jobs=-1)  # M_YME
     elif Param == "M_PR":
-        modelRF = RandomForestRegressor(bootstrap=False, max_depth=10, max_features="auto", min_samples_leaf=1, min_samples_split=4, n_estimators=100, random_state=7)  # M_PR
-        modelCAT = CatBoostRegressor(depth=7, iterations=500, learning_rate=0.01) # M_PR
-        modelXGB = xgboost.XGBRegressor(colsample_bytree=1, gamma=0, learning_rate=0.15, max_depth=6, min_child_weight=0, n_estimators=100, reg_alpha=None, reg_lambda=None, subsample=1) # M_PR
-        modelKNN = KNeighborsRegressor(n_neighbors=6, n_jobs=-1) # M_PR
+        modelRF = RandomForestRegressor(bootstrap=False, max_depth=10, max_features="auto", min_samples_leaf=1,
+                                        min_samples_split=4, n_estimators=100, random_state=7)  # M_PR
+        modelCAT = CatBoostRegressor(depth=7, iterations=500, learning_rate=0.01)  # M_PR
+        modelXGB = xgboost.XGBRegressor(colsample_bytree=1, gamma=0, learning_rate=0.15, max_depth=6,
+                                        min_child_weight=0, n_estimators=100, reg_alpha=None, reg_lambda=None,
+                                        subsample=1)  # M_PR
+        modelKNN = KNeighborsRegressor(n_neighbors=6, n_jobs=-1)  # M_PR
     modelLR = LinearRegression(normalize=False, fit_intercept=True)
     # modelEL = ElasticNet(normalize=False, fit_intercept=True)
 
- # Поиск по сетке параметров модели случайного леса
- #    random_grid = {
- # "bootstrap" : [False, True],
- # 'max_features': ['auto', 'sqrt'],
- # 'max_depth': [5,8,10,12,15,None],
- # 'max_features': ['auto', 'sqrt'],
- # 'min_samples_leaf': [1,2,3,4,5,6],
- # 'min_samples_split': [2,3,4,5,6,7,8],
- # 'n_estimators': [100]}
+    LeaveOneOut_Onemodel(X, y, modelLR)
 
-    # catboost_grid = {"depth" : [4,5,8,10,15], "learning_rate" : [0.01, 0.02, 0.03, 0.04, 0.05], "iterations" : [300,500]}
-    # xgboost_grid = {
-    #    'colsample_bytree':[0.4,0.6,0.8],
-    #    'gamma':[0,0.03,0.1,0.3],
-    #    'min_child_weight':[0,1,5,15],
-    #    'learning_rate':[0.05, 0.1, 0.3],
-    #    'max_depth':[3,4,5],
-    #    'n_estimators':[200],
-    #    'reg_alpha':[1e-5, 0.75, None],
-    #    'reg_lambda':[1e-5, 0.45, None],
-    #    'subsample':[0.04, 0.6, 0.95, 2]}
-    # grid = GridSearchCV(model, xgboost_grid, n_jobs=-1, cv=5)
-    # grid.fit(X,y)
-    # print(grid.best_params_)
-    # {'iterations': 200, 'learning_rate': 0.01, 'max_depth': 5} UCS
-    # {'depth': 5, 'iterations': 200, 'l2_leaf_reg': 1, 'learning_rate': 0.01} UCS2
-    # {'depth': 15, 'iterations': 500, 'l2_leaf_reg': 1, 'learning_rate': 0.05} TSTR
-    # {'depth': 10, 'iterations': 200, 'learning_rate': 0.03} FANG
-    # {'depth': 5, 'iterations': 200, 'learning_rate': 0.01} A_YME
-    # {'depth': 15, 'iterations': 200, 'learning_rate': 0.01} A_PR
-    # {'depth': 5, 'iterations': 300, 'learning_rate': 0.03} M_YME
-    # {'depth': 4, 'iterations': 500, 'learning_rate': 0.04} M_YME2
-    # {'depth': 4, 'iterations': 500, 'learning_rate': 0.02} M_PR
-    # {'colsample_bytree': 0.4, 'gamma': 0.3, 'learning_rate': 0.07, 'max_depth': 3, 'min_child_weight': 10, 'n_estimators': 200, 'reg_alpha': 0.75, 'reg_lambda': 0.45, 'subsample': 0.6} UCS par
-    # {'colsample_bytree': 0.6, 'gamma': 0.3, 'learning_rate': 0.1, 'max_depth': 5, 'min_child_weight': 1.5, 'n_estimators': 200, 'reg_alpha': 1e-05, 'reg_lambda': 1e-05, 'subsample': 0.6} TSTR par
-    # {'colsample_bytree': 0.6, 'gamma': 0.3, 'learning_rate': 0.1, 'max_depth': 4, 'min_child_weight': 1.5, 'n_estimators': 200, 'reg_alpha': 0.75, 'reg_lambda': 0.01, 'subsample': 0.95} FANG par
-    # {'colsample_bytree': 0.4, 'gamma': 0.03, 'learning_rate': 0.05, 'max_depth': 5, 'min_child_weight': 0, 'n_estimators': 200, 'reg_alpha': 0.75, 'reg_lambda': None, 'subsample': 0.04} A_YME par
-    # {'colsample_bytree': 0.4, 'gamma': 0, 'learning_rate': 0.05, 'max_depth': 3, 'min_child_weight': 15, 'n_estimators': 200, 'reg_alpha': 1e-05, 'reg_lambda': 1e-05, 'subsample': 0.6} A_PR par
-    # {'colsample_bytree': 0.8, 'gamma': 0.03, 'learning_rate': 0.3, 'max_depth': 5, 'min_child_weight': 0, 'n_estimators': 200, 'reg_alpha': None, 'reg_lambda': 1e-05, 'subsample': 0.6} M_YME
-    # {'colsample_bytree': 0.8, 'gamma': 0, 'learning_rate': 0.1, 'max_depth': 3, 'min_child_weight': 0, 'n_estimators': 200, 'reg_alpha': None, 'reg_lambda': None, 'subsample': 0.95} M_PR
-
-    # LeaveOneOut for one model
-    loo = LeaveOneOut()
-    loo.get_n_splits(X)
-    Prediction = []
-    for train_index, test_index in loo.split(X):
-        X_train, X_test = X.iloc[train_index, :], X.iloc[test_index,:]
-        y_train, y_test = y[train_index], y[test_index]
-        modelXGB.fit(X_train, y_train)
-        pre = modelXGB.predict(X_test)[0]
-        print(pre)
-        Prediction.append(pre)
-    pred = pd.Series(Prediction)
-    SST = sum((y - y.mean())**2)
-    SSE = sum((pred - y)**2)
-    print("LLO", 1-SSE/SST)
-
-    # LeaveOneOut for ensembles
+    # LeaveOneOut for bagging
     # loo = LeaveOneOut()
     # loo.get_n_splits(X)
     # Prediction = []
@@ -430,7 +384,7 @@ def dataset_former_acoustic(FileName, Param):
     # SSE = sum((pred - y) ** 2)
     # print("LLO", 1 - SSE / SST)
 
-    # Boosting2
+    # Leave One Out for boosting 2
     # loo = LeaveOneOut()
     # loo.get_n_splits(X)
     # Prediction = []
@@ -475,25 +429,22 @@ def dataset_former_acoustic(FileName, Param):
     # print("LLO", 1 - SSE / SST)
 
 
-
-    # Модель линейной регрессии
-    # lm = LinearRegression(normalize=True, fit_intercept=True)
-    # print("Кросс-валидация линейной модели: " + str(cross_val_score(lm, X, y, cv=5, scoring="r2").mean()))
-    # lm.fit(X_train, y_train)
-    # print("Отложенная выборка: " + str(lm.score(X_test, y_test)))
-
-    # Случайный лес
-    # rf = RandomForestRegressor(bootstrap=True, max_depth=15, max_features="auto", min_samples_leaf=5, min_samples_split=2, n_estimators=100, random_state=7)
-    # print("Кросс-валидация случайного леса: " + str(cross_val_score(rf, X, y, cv=5, scoring="r2").mean()))
-    # rf.fit(X_train, y_train)
-    # print("Отложенная выборка: " + str(rf.score(X_test, y_test)))
-
-    # CatBoostRegressor
-    # model = CatBoostRegressor(iterations=4, learning_rate=1, depth=4)
-    # model.fit(X_train, y_train)
-    # print(model.score(X_test, y_test))
-
-    # return Data
+def LeaveOneOut_Onemodel(X,y, model):
+    # LeaveOneOut for one model
+    loo = LeaveOneOut()
+    loo.get_n_splits(X)
+    Prediction = []
+    for train_index, test_index in loo.split(X):
+        X_train, X_test = X.iloc[train_index, :], X.iloc[test_index, :]
+        y_train, y_test = y[train_index], y[test_index]
+        model.fit(X_train, y_train)
+        pre = model.predict(X_test)[0]
+        print(pre)
+        Prediction.append(pre)
+    pred = pd.Series(Prediction)
+    SST = sum((y - y.mean()) ** 2)
+    SSE = sum((pred - y) ** 2)
+    print("LLO", 1 - SSE / SST)
 
 def dataset_former_gamma(FileName, Param):
 
@@ -750,8 +701,22 @@ def dataset_former_gamma(FileName, Param):
 # dataset_former_acoustic("AcousticLearn.xlsx", "A_YME")
 # dataset_former_acoustic("AcousticLearn.xlsx", "A_PR")
 
+def vizualization(FileName, Param):
 
-dataset_former_gamma("GammaLearnNew.xlsx", "TSTR")
+    Data = dataset_former_acoustic(FileName, Param)
+    plt.hist(Data["depth"], bins=10)
+    plt.show()
+
+    Data.boxplot(column=[Param])
+    plt.show()
+
+
+# acoustic_learn("AcousticLearn.xlsx", "UCS")
+# vizualization("AcousticLearn.xlsx", "UCS")
+
+data = dataset_former_acoustic("AcousticLearn.xlsx", "UCS")
+# print(data)
+
 # b = dataset_former_gamma("GammaLearnNew.xlsx", "TSTR")
 # c = dataset_former_gamma("GammaLearnNew.xlsx", "FANG")
 # d = dataset_former_gamma("GammaLearnNew.xlsx", "A_YME")
